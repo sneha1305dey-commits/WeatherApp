e7236e5d8a14e6fffca460eb61e0e9bace796735
sneha1305dey-commits <sneha1305dey@gmail.com>
2026-08-23 23:43:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', ()=>{
+    const location = document.getElementById('location').value;
+
+    function updateTemp(data) {
+        const element = document.getElementById('weatherInfo');
+        element.innerHTML = `Today's Temperature: ${data.current.temp_c}`;
+    }
+
+
+    const prom = fetch(`http://api.weatherapi.com/v1/current.json?key=a00738652512433c8ae175813260108&q=${location}&aqi=no`);
+
+    prom
+    .then((response)=>response.json())
+    .then((data)=> updateTemp(data));
+})

2c26d8d3f31b9147e2a7518b6404918ecb3c82c7
sneha1305dey-commits <sneha1305dey@gmail.com>
2026-09-05 18:05:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const btn = document.querySelector('button');
 
-btn.addEventListener('click', ()=>{
+btn.addEventListener('click', () => {
     const location = document.getElementById('location').value;
 
     function updateTemp(data) {
         const element = document.getElementById('weatherInfo');
-        element.innerHTML = `Today's Temperature: ${data.current.temp_c}`;
+
+        element.innerHTML = `Today's Temperature: ${data.current.temp_c}°C`;
     }
 
-
-    const prom = fetch(`https://api.weatherapi.com/v1/current.json?key=a00738652512433c8ae175813260108&q=${location}&aqi=no`);
+    const prom = fetch(
+        `https://api.weatherapi.com/v1/current.json?key=a00738652512433c8ae175813260108&q=${location}&aqi=no`
+    );
 
     prom
-    .then((response)=>response.json())
-    .then((data)=> updateTemp(data));
-})
+        .then((response) => response.json())
+        .then((data) => updateTemp(data));
+});
